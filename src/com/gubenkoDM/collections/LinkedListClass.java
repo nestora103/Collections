a508d69c8d029d4d7936ee83d291068a7e3cbca6
nestora103 <nestora103@gmail.com>
2017-03-23 17:05:30 +0300

    LinkedList test
    LinkedList test

---
+++ src/com/gubenkoDM/collections/LinkedListClass.java
@@ -0,0 +1,28 @@
+package com.gubenkoDM.collections;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+/**
+ * Created by DmitriX on 23.03.2017.
+ */
+public class LinkedListClass {
+    private void printCollection(Collection<Cat> collection){
+        //получаем имя класса через рефлексию
+        System.out.println(collection.getClass().getSimpleName());
+        for (Cat cat:collection) {
+            System.out.println(cat.getSize()+" "+cat.getAge());
+        }
+    }
+    public void test(){
+        LinkedList<Cat> ll=new LinkedList<>();
+        ll.add(new Cat(1,1));
+        printCollection(ll);
+
+        ll.add(0,new Cat(2,2));
+        printCollection(ll);
+
+
+
+    }
+}
